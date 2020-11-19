4a247ec15b671631b374b021ed1e364a5d79dcb3
yourname1057286357@qq.com <1057286357@qq.com>
2020-11-20 01:10:59 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 1、安装插件
+Vue.use(VueRouter)
+//2、创建router 
+const routes = [
+// 配置对应关系
+]
+const router = new Vue.VueRouter({
+    routes,
+    mode:'history'
+})
+
+
+export default router
